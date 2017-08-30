d09d6fa8cae8bf20703b16aafa43d6bbd98fab5f
gizmore <gizmore@wechall.net>
2017-08-30 19:40:26 +0200

    close_to_a_working_classic_theme

--- tpl/listitem/address.php
+++ tpl/listitem/address.php
@@ -3,9 +3,9 @@ use GDO\Address\GDO_Address;
 $address instanceof GDO_Address;
 $id = $address->getID();
 ?>
-<md-list-item class="md-2-line" ng-click="null" href="<?= href('Address', 'Crud', '&id='.$id); ?>">
+<li class="md-list-item md-2-line" ng-click="null" href="<?= href('Address', 'Crud', '&id='.$id); ?>">
   <div class="md-list-item-text" layout="column">
     <h3><?= html($address->getStreet()); ?></h3>
     <p><?= html(t('address_line', [$address->getZIP(), $address->getCity(), $address->getCountry()->displayName()])); ?></p>
   </div>
-</md-list-item>
+</li>

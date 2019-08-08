01fef552e4cafaff13c0c6c8c6d0a36d7f007c8a
gizmore <gizmore@wechall.net>
2019-08-08 23:11:33 +0200

    6.10-r11

--- Module_Address.php
+++ Module_Address.php
@@ -6,6 +6,6 @@ use GDO\Core\GDO_Module;
 final class Module_Address extends GDO_Module
 {
 	public $module_priority = 10;
-	public function getClasses() { return ['GDO\Address\GDO_Address']; }
+	public function getClasses() { return ['GDO\\Address\\GDO_Address']; }
 	public function onLoadLanguage() { $this->loadLanguage('lang/address'); }
 }

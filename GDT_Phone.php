8b5612db4d1752b95e3c05ee1dc52bcc41bc7dee
gizmore <gizmore@wechall.net>
2021-05-06 01:22:24 +0200

    6.10.2-r1071

--- GDT_Phone.php
+++ GDT_Phone.php
@@ -2,13 +2,16 @@
 namespace GDO\Address;
 
 use GDO\DB\GDT_String;
-use GDO\Core\GDT;
 
 /**
  * A phone number.
+ * 
  * @TODO validate existing country phone codes. validate plausible length.
  * @TODO write a phone-validator module that uses gdo6-sms to validate a phone.
+ * 
  * @author gizmore
+ * @version 6.10.1
+ * @since 6.8.0
  */
 final class GDT_Phone extends GDT_String
 {
@@ -19,13 +22,13 @@ final class GDT_Phone extends GDT_String
 	    parent::__construct();
 		$this->min = 7;
 		$this->max = 20;
-		$this->pattern = "#^\\+?[-/0-9 ]+$#";
+		$this->pattern = "#^\\+?[-/0-9 ]+$#Ds";
 		$this->encoding = self::ASCII;
 	}
 	
 	public function plugVar()
 	{
-	    return '+49 176 59 59 88 44';
+	    return '+49 176 / 59 59 88 44';
 	}
 	
 }
